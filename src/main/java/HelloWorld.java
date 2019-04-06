8d1f324d69488f4181b2a04bbbd40ead27f661bd
一秋 <liyuanyong@itcast.cn>
2019-04-06 21:57:41 +0800

    修改成4了

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
-        System.out.println("helloworld3");
+        System.out.println("helloworld4");
     }
 }
